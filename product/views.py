050fd640d6ebc9da2393ae7814237bdb712bf61b
luis <leluizedson@gmail.com>
2025-08-31 01:05:54 -0300

    first view and template

--- product/views.py
+++ product/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Product
 
 def index(request):
-    return HttpResponse('Hello world')
+    latest_product_list = Product.objects.order_by('-stock')
+    context = {
+        'latest_product_list': latest_product_list
+        }
+    return render(request, 'product/index.html', context)
+
+def detail(request, product_id):
+    return HttpResponse("This is the product formerly known as: %s" % product_id)
