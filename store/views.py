914557243a2ab10df93f8cbf07128442ae4e4136
luis <leluizedson@gmail.com>
2025-10-19 09:31:53 -0300

    included paginator

--- store/views.py
+++ store/views.py
@@ -1,6 +1,14 @@
+from django.core.paginator import Paginator
 from django.shortcuts import render
 from product.models import Product
 
 def  index(request):
-    products = Product.objects.all()
+    products_list = Product.objects.all().order_by('name')
+
+    paginator = Paginator(products_list, 9)
+
+    page_number = request.GET.get('page')
+
+    products = paginator.get_page(page_number)
+
     return render(request, 'store/index.html', {'products':products})
