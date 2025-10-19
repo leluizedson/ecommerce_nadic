0e7e2f17a2f7de8b1da621cfd2d8af7b05aae0b7
luis <leluizedson@gmail.com>
2025-10-19 20:50:59 -0300

    included search logic

--- store/views.py
+++ store/views.py
@@ -3,12 +3,17 @@ from django.shortcuts import render
 from product.models import Product
 
 def  index(request):
+    query = request.GET.get('q')
+
     products_list = Product.objects.all().order_by('name')
 
+    if query:
+        products_list = products_list.filter(name__icontains=query)
+    
     paginator = Paginator(products_list, 9)
 
     page_number = request.GET.get('page')
 
     products = paginator.get_page(page_number)
 
-    return render(request, 'store/index.html', {'products':products})
+    return render(request, 'store/index.html', {'products':products, 'query': query,})
