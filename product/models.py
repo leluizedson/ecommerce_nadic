59c7cb714616610b9800015ed89ced91e94da566
luis <luis@email.com>
2025-10-22 07:42:18 -0300

    added imafes to products

--- product/models.py
+++ product/models.py
@@ -7,6 +7,13 @@ class Product(models.Model):
     price = models.DecimalField(max_digits=10, decimal_places=2)
     stock = models.IntegerField(blank=False)
 
+
     def __str__(self):
         return self.name
     
+class ImageProduct(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='images')
+    image = models.ImageField(upload_to='product_images/')
+
+    def __str__(self):
+        return f"Image for {self.product.name}"
