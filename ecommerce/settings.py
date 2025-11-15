9da74a618a1b248e9c61d9f69d5777d8b0b7ad0f
luis <leluizedson@gmail.com>
2025-11-15 20:03:20 -0300

    fix order number bug

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -79,12 +79,8 @@ WSGI_APPLICATION = 'ecommerce.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'ecommerce_db',
-        'USER': 'postgres',
-        'PASSWORD': 'sql',
-        'HOST': 'db',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
--- order/models.py
+++ order/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 from django.conf import settings 
 from product.models import Product
-from cart.models import CartItem
 
 class Order(models.Model):
     STATUS_CHOICES = [
@@ -12,13 +11,22 @@ class Order(models.Model):
     ]
 
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    order_number = models.PositiveIntegerField(null=True, blank=True)  # novo campo
     created_at = models.DateTimeField(auto_now_add=True)
     status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='pending')
     total = models.DecimalField(max_digits=10, decimal_places=2, default=0)
 
+    def save(self, *args, **kwargs):
+        # gera order_number por cliente
+        if self.order_number is None:
+            last_order = Order.objects.filter(user=self.user).order_by('-order_number').first()
+            self.order_number = 1 if not last_order else last_order.order_number + 1
+        super().save(*args, **kwargs)
+
     def __str__(self):
-        return f'Order #{self.id} - {self.user.username}'
+        return f"Order #{self.order_number} - {self.user.username}"
     
+
 class OrderItem(models.Model):
     order = models.ForeignKey(Order, on_delete=models.CASCADE, related_name='items')
     product = models.ForeignKey(Product, on_delete=models.SET_NULL, null=True)
